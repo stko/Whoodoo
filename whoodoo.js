0b0950fd748c199446340cd54535d385efe89a9e
Steffen Köhler <steffen@koehlers.de>
2019-03-09 18:00:25 +0100

    URLs in Values are now clickable

--- whoodoo.js
+++ whoodoo.js
@@ -324,10 +324,16 @@ $(function () {
 						return false;
 				}
 			},
-			formatURL: function (value) {
-				return "URL:" + value;
+			checkIfURL: function (value) {
+				//https://stackoverflow.com/a/5717133
+				var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+				'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+				'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+				'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+				'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+				'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+			  return !!pattern.test(value);
 			}
-
 		}
 	});
 
